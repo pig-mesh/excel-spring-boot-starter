54ea99c42adad206e88cbe9c3947ead520c9ff8a
林恬伟 <lintw@kamfu.com.cn>
2022-08-29 18:21:39 +0800

    主要解决Validator验证和业务逻辑验证导致返回给前端时行号错误的问题 添加一个注解用来在vo类中标识存储行号的字段 修改DefaultAnalysisEventListener判断vo类中是否存在被注解标识的字段,并尝试将当前行号写入

---
+++ src/main/java/com/pig4cloud/plugin/excel/annotation/ExcelLine.java
@@ -0,0 +1,9 @@
+package com.pig4cloud.plugin.excel.annotation;
+
+import java.lang.annotation.*;
+
+@Documented
+@Target({ ElementType.FIELD })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExcelLine {
+}
